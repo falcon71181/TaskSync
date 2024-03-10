9fca96e9bdfa44fe768e6981ed17539efe9a6a94
falcon71181 <abhaythakur71181@gmail.com>
2024-03-10 13:05:59 +0530

    get all tasks & delete task API
    getAllTasks & deleteTask controller + route Added

--- server/src/controllers/task.controller.ts
+++ server/src/controllers/task.controller.ts
@@ -1,6 +1,25 @@
 import { Request, RequestHandler, Response } from "express";
 import { TaskModel } from "../models/task.model";
 
+// get all tasks
+const getAllTasks: RequestHandler = async (req: Request, res: Response) => {
+  try {
+    const email = (req as Request & { email: string }).email;
+    const tasks = await TaskModel.find({ userEmail: email });
+
+    if (tasks.length === 0) {
+      const error = "No task found";
+      res.status(404).send({ error: error });
+    }
+
+    res.status(200).send(tasks);
+  } catch (error) {
+    // Handle errors
+    console.error("Error getting tasks:", error);
+    res.status(500).send({ error: "Internal server error." });
+  }
+};
+
 // create task
 const createTask: RequestHandler = async (req: Request, res: Response) => {
   try {
@@ -35,4 +54,39 @@ const createTask: RequestHandler = async (req: Request, res: Response) => {
   }
 };
 
-export { createTask };
+// delete task
+const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const email = (req as Request & { email: string }).email;
+    const { taskId } = req.params;
+
+    const task = await TaskModel.findById(taskId);
+
+    if (!task) {
+      const error = "Task not found.";
+      return res.status(404).send({ error: error });
+    }
+
+    // only same user can continue
+    if (task?.userEmail != email) {
+      const error = "You donot have permissions.";
+      return res.status(401).send({ error: error });
+    }
+
+    // deleting the task
+    const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+    if (!deletedTask) {
+      const error = "Task not found. Invalid task id";
+      return res.status(404).send({ error });
+    }
+
+    res.status(200).send({ message: "Task deleted successfully." });
+  } catch (error) {
+    // Handle errors
+    console.error("Error registering user:", error);
+    res.status(500).send({ error: "Internal server error." });
+  }
+};
+
+export { getAllTasks, createTask, deleteTask };
--- server/src/routes/task.route.ts
+++ server/src/routes/task.route.ts
@@ -1,11 +1,23 @@
 import { Router, type IRouter } from "express";
 import { isAuth } from "../middleware/authReq";
-import { createTask } from "../controllers/task.controller";
+import {
+  getAllTasks,
+  createTask,
+  deleteTask,
+} from "../controllers/task.controller";
 
 const router: IRouter = Router();
+
+// get all tasks
+// /tasks/
+router.get("/", isAuth, getAllTasks);
 
 // create new task
 // /tasks/create
 router.post("/create", isAuth, createTask);
+
+// delete particular task
+// /tasks/delete
+router.delete("/:taskId/delete", isAuth, deleteTask);
 
 export { router as task_router };
