51b4e5ba2101cc6d839ebc44c8feb07f7fa4ab63
falcon71181 <abhaythakur71181@gmail.com>
2024-03-11 10:14:17 +0530

    Vercel Deploy Fixes

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -4,6 +4,11 @@ import { task_router } from "./task.route";
 
 const router: IRouter = Router();
 
+// redirect to health api
+router.get("/", (_req, res) => {
+  res.redirect("/health");
+});
+
 // health check route
 router.get("/health", (_req, res) => {
   res.sendStatus(200);
