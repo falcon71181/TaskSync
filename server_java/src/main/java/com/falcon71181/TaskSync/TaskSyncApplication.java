60d7f230da34b734e4593b95beb690e9a507557d
falcon71181 <abhaythakur71181@gmail.com>
2024-06-18 14:18:46 +0530

    feat: initialize spring boot

---
+++ server_java/src/main/java/com/falcon71181/TaskSync/TaskSyncApplication.java
@@ -0,0 +1,13 @@
+package com.falcon71181.TaskSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSyncApplication.class, args);
+	}
+
+}
---
+++ server_java/src/test/java/com/falcon71181/TaskSync/TaskSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falcon71181.TaskSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSyncApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
